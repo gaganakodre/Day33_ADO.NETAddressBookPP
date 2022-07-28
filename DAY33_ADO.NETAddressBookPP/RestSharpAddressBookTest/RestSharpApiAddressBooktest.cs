d4d42b5705dc0764139089f491fba0e9f932bf56
shreegowri <gaganandh@gmail.com>
2022-07-29 00:12:48 +0530

    [shreegowri]implemented the add contacts using rest sharp

--- DAY33_ADO.NETAddressBookPP/RestSharpAddressBookTest/RestSharpApiAddressBooktest.cs
+++ DAY33_ADO.NETAddressBookPP/RestSharpAddressBookTest/RestSharpApiAddressBooktest.cs
@@ -1,6 +1,7 @@
 using Newtonsoft.Json;
 using RestSharp;
 using System.Net;
+using System.Text.Json.Nodes;
 
 namespace RestSharpAddressBook
 {
@@ -46,6 +47,33 @@ namespace RestSharpAddressBook
             {
                 System.Console.WriteLine("id: " + addressBook.Id + ",FirstName: " + addressBook.FirstName + ",LastName: " + addressBook.LastName + ",Address" + addressBook.Address + ",Ciyt" + addressBook.City + ",State" + addressBook.State + ",Phonenumber" + addressBook.PhoneNumber + ",Zip" + addressBook.Zip + ",Email" + addressBook.Email);
             }
+        }
+        [Test]
+        public void givenContacts_OnPost_ShouldReturnAddedAddressBook()
+        {
+            RestRequest request = new RestRequest("/AddressBook", Method.Post);
+            JsonObject jObjectbody = new JsonObject();
+            jObjectbody.Add("FirstName", "Ganesh");
+            jObjectbody.Add("LastName", "jhonny");
+            jObjectbody.Add("PhoneNumber", "9187654344");
+            jObjectbody.Add("Address", "Ganapathinagara");
+            jObjectbody.Add("City", "wyizag");
+            jObjectbody.Add("State", "Ap");
+            jObjectbody.Add("Zip", "1234");
+            jObjectbody.Add("email", "ganesha@gmail.com");
+            request.AddParameter("application/json", jObjectbody, ParameterType.RequestBody);
+            RestResponse response = client.Execute(request);
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+            AddressBook dataResponse = JsonConvert.DeserializeObject<AddressBook>(response.Content);
+            Assert.AreEqual("Ganesh", dataResponse.FirstName);
+            Assert.AreEqual("jhonny", dataResponse.LastName);
+            Assert.AreEqual("9187654344", dataResponse.PhoneNumber);
+            Assert.AreEqual("Ganapathinagara", dataResponse.Address);
+            Assert.AreEqual("wyizag", dataResponse.City);
+            Assert.AreEqual("Ap", dataResponse.State);
+            Assert.AreEqual("1234", dataResponse.Zip);
+            Assert.AreEqual("ganesha@gmail.com", dataResponse.Email);
+
         }
     }
 }
